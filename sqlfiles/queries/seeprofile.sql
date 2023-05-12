48da6c8c1a9105c5c64f09c5a1d92775ac3fb74d
viswatara <99180943+viswatara@users.noreply.github.com>
2023-05-12 14:31:20 +0530

    Update seeprofile.sql

--- sqlfiles/queries/seeprofile.sql
+++ sqlfiles/queries/seeprofile.sql
@@ -1,14 +1,21 @@
 set serveroutput on
 declare
     regNo Student.regNo%TYPE;
-    name Student.name%TYPE;
+    stud_name Student.stud_name%TYPE;
     email Student.email%TYPE;
     phone Student.phone%TYPE;
 begin
     regNo := '&RegistrationNumber';
-    select name into name from Student where regNo = regNo;
-    select name into email from Student where regNo = regNo;
-    select name into phone from Student where regNo = regNo;
+    select stud_name into stud_name from Student where regNo = regNo;
+    select email into email from Student where regNo = regNo;
+    select phone into phone from Student where regNo = regNo;
     dbms_output.put_line('Registration Number: ' || regNo || ' Name: ' || name || ' Email: ' || email || ' Phone: ' || phone);
 end;
 /
+SET @regNo = 'RegistrationNumber';
+
+SELECT stud_name, email, phone INTO @name, @email, @phone
+FROM Student
+WHERE regNo = @regNo;
+
+SELECT CONCAT('Registration Number: ', @regNo, ' Name: ', @name, ' Email: ', @email, ' Phone: ', @phone);
