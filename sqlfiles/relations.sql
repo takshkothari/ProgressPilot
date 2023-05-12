b921a76f211aff44ada5768b7606e9fe5938a676
viswatara <99180943+viswatara@users.noreply.github.com>
2023-05-12 14:03:37 +0530

    Update relations.sql

--- sqlfiles/relations.sql
+++ sqlfiles/relations.sql
@@ -7,7 +7,7 @@ delete from belongs;
 delete from sem;
 
 insert into student values (210905338, 'Taksh', 'taksh@gmail.com', 'taksh1234', '913632','1980-01-01');
-insert into student values (210905170, 'Viswatara', 'viswatara@gmail.com', 'tara5678', '6281869387');
+insert into student values (210905170, 'Viswatara', 'viswatara@gmail.com', 'tara5678', '62818697','2003-09-25');
 
 insert into course values ('c1', 'DBS', 3);
 insert into course values ('c2', 'DAA', 4);
@@ -16,9 +16,9 @@ insert into course values ('c4', 'FLAT', 3);
 insert into course values ('c5', 'MATH', 3);
 insert into course values ('c6', 'OE', 3);
 
-insert into assignment values ('a1', 'c2', 10);
-insert into assignment values ('a2', 'c2', 10);
-insert into assignment values ('a3', 'c1', 20);
+insert into assignment values ('a1', 10,'c2');
+insert into assignment values ('a2', 20,'c2');
+insert into assignment values ('a3', 20,'c1');
 
 insert into enrolls values (210905170, 'c1', 4, 'A');
 insert into enrolls values (210905338, 'c2', 4, 'A+');
