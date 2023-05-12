701dec36c6edcfacd9643ccf3c9b89d8a8f6b4c6
viswatara <99180943+viswatara@users.noreply.github.com>
2023-05-12 14:24:06 +0530

    Update addass.sql

--- sqlfiles/queries/addass.sql
+++ sqlfiles/queries/addass.sql
@@ -12,3 +12,11 @@ begin
     dbms_output.put_line('Assignment Added Successfully');
 end;
 /
+
+SET @ae_id = 'AssignmentID';
+SET @mm = 'MaxMarks';
+SET @cid = 'CourseID';
+
+INSERT INTO assignment (ae_id, maxmarks, course_id) VALUES (@ae_id, @mm, @cid);
+
+SELECT 'Assignment Added Successfully';
